de89f8a22473124a2a98b2c22b29a0559cad1429
bamamounicolas <bamamounicolas@gmail.com>
2023-07-23 01:18:36 +0800

    My first commit of ESP32 S3 Screen

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "E:/ESP/ToolChain/Espressif/frameworks/esp-idf-v5.0.2/components/bootloader/subproject"
+  "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader"
+  "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader-prefix"
+  "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader-prefix/tmp"
+  "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader-prefix/src/bootloader-stamp"
+  "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader-prefix/src"
+  "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "E:/ZX7D00CE01S-master/ESP32 S3 Screen/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
